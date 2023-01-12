4c6bede4b2bd34e66e257f0979b648f9f0b13af6
syahfikryarendas <syahfikryarendas@gmail.com>
2023-01-12 08:21:37 +0700

    FIRST COMMIT

---
+++ main.py
@@ -0,0 +1,38 @@
+from Model.daftar_nilai import *
+from View.view_nilai import *
+
+#Mulai
+print("===========================================")
+print("               PROGRAM 1                   ")
+print("NAMA         : SYAHFIKRY ARENDAS           ")
+print("NIM          : 312210617                   ")
+print("KELAS        : TI.22.A1                    ")
+print("===========================================")
+
+while True:
+    print("\n")
+    menu = input("(L) Lihat, (T) Tambah, (H) Hapus, (U) Ubah, (C) Cari, (K) Keluar\nPilih menu: ")
+    print("\n")
+
+    # menu
+    if menu.lower() == 't':
+        tambah_data()
+
+    elif menu.lower() == 'c':
+        cari_data()
+
+    elif menu.lower() == 'l':
+        lihat_data()
+
+    elif menu.lower() == 'u':
+        ubah_data()
+
+    elif menu.lower() == 'h':
+        hapus_data()
+
+    # Keluar
+    elif menu.lower() == 'k':
+        break
+
+    else:
+        print("Ada yang salah, silahkan cek kembali.")
